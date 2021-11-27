8daee81939aabb341b7ca9f666de77fdc68da791
Marvin W <marvwood12@gmail.com>
2021-11-27 14:40:37 -0500

    theme buttton functionality

--- app.js
+++ app.js
@@ -18,19 +18,19 @@ for (let i = 0; themeDots.length > i; i++){
 
 function setTheme(mode){
     if(mode === 'light'){
-        document.getElementById('theme-style').href = 'default.css'
+        document.getElementById('theme-style').href = '/default.css'
     }
 
     if(mode === 'blue'){
-        document.getElementById('theme-style').href = 'blue.css'
+        document.getElementById('theme-style').href = 'themes/blue.css'
     }
 
     if(mode === 'green'){
-        document.getElementById('theme-style').href = 'green.css'
+        document.getElementById('theme-style').href = 'themes/green.css'
     }
 
     if(mode === 'purple'){
-        document.getElementById('theme-style').href = 'purple.css'
+        document.getElementById('theme-style').href = 'themes/purple.css'
     }
 
     localStorage.setItem('theme', mode);
